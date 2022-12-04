296ae11a12fb089e40252fba129b19ed8c43c924
TheSpaceHuman <kolodyazhnyartem@gmail.com>
2022-12-04 16:51:56 +0300

    add stylelint

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -4,11 +4,6 @@ type Data = {
   name: string;
 };
 
-const f = () => {
-  console.debug(1);
-};
-
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  f();
   res.status(200).json({ name: 'John Doe' });
 }
